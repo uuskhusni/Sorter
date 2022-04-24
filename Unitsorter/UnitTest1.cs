e569ff0f5f967700fd2f957cfb5ba49420e6b0a9
uus khusni <uuskhusni@gmail.com>
2022-04-24 22:29:20 +0700

    Upload Project

---
+++ Unitsorter/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using sorter;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Unitsorter
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            sorter.Sorter.sorterandprintProcess("unsorted-names-list.txt");
+        }
+    }
+}
